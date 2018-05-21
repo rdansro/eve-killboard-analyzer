6e142ca19ceb829e028a17a8bfe0a43cb61163fa
coopss <rcooper@nmt.edu>
2018-05-20 21:34:50 -0600

    Updated typeid source and manually added all ships to classes (need to automate this for future updates)

---
+++ src/ship_classes.py
@@ -0,0 +1,597 @@
+ASSAULT_FRIGATE = [
+        '11383', # Gatherer
+        '11393', # Retribution
+        '11365', # Vengeance
+        '11381', # Harpy
+        '11379', # Hawk
+        '12044', # Enyo
+        '11375', # Erinye
+        '12042', # Ishkur
+        '11389', # Kishar
+        '11373', # Blade
+        '12036', # Dagger
+        '11400', # Jaguar
+        '11371',  # Wolf
+        '32788', # Cambion
+        '32207', # Freki
+        '3516', # Malice
+        '2834'  # Utu
+
+    ]
+
+ATTACK_BATTLECRUISER = [
+        '4302', # Oracle
+        '4306', # Naga
+        '4308', # Talos
+        '4310'  # Tornado
+    ]
+
+BATTLESHIP = [
+        '24692', # Abaddon
+        '33625', # Abaddon Kador Edition
+        '33623', # Abaddon Tash-Murkon Edition
+        '642',   # Apocalypse
+        '34213', # Apocalypse Blood Raider Edition
+        '11936', # Apocalypse Imperial Issue
+        '34215', # Apocalypse Kador Edition
+        '34217', # Apocalypse Tash-Murkon Edition
+        '643',   # Armageddon
+        '11938', # Armageddon Imperial Issue
+        '34151', # Rattlesnake Victory Edition
+        '638',   # Raven
+        '34225', # Raven Guristas Edition
+        '34227', # Raven Kaalakiota Edition
+        '34229', # Raven Nugoeihuvi Edition
+        '26840', # Raven State Issue
+        '24688', # Rokh
+        '33627', # Rokh Nugoeihuvi Edition
+        '33629', # Rokh Wiyrkomi Edition
+        '640',   # Scorpion
+        '4005',  # Scorpion Ishukone Watch
+        '645',   # Dominix
+        '24690', # Hyperion
+        '33635', # Hyperion Aliastra Edition
+        '33637', # Hyperion Inner Zone Shipping Edition
+        '641',   # Megathron
+        '13202', # Megathron Federate Issue
+        '34239', # Megathron Inner Zone Shipping Edition
+        '34237', # Megathron Police Edition
+        '34118', # Megathron Quafe Edition
+        '24694', # Maelstrom
+        '33633', # Maelstrom Krusual Edition
+        '33631', # Maelstrom Nefantar Edition
+        '639',   # Tempest
+        '34247', # Tempest Justice Edition
+        '34249', # Tempest Krusual Edition
+        '34251', # Tempest Nefantar Edition
+        '26842', # Tempest Tribal Issue
+        '644',    # Typhoon
+        '1918', # Concord Army Battleship
+        '1912', # Concord Police Battleship
+        '1914', # Concord Special Ops Battleship
+        '1916',  # Concord SWAT Battleship
+        '17726', # Apocalypse Navy Issue
+        '32305', # Armageddon Navy Issue
+        '17636', # Raven Navy Issue
+        '32309', # Scorpion Navy Issue
+        '32307', # Dominix Navy Issue
+        '17728', # Megathron Navy Issue
+        '17732', # Tempest Fleet Issue
+        '32311',  # Typhoon Fleet Issue
+        '17738', # Machariel
+        '17920', # Bhaalgorn
+        '17918', # Rattlesnake
+        '33820', # Barghest
+        '17736', # Nightmare
+        '17740', # Vindicator
+        '33472'  # Nestor
+    ]
+
+BLACK_OPS = [
+        '22428', # Redeemer
+        '22436', # Widow
+        '22430', # Sin
+        '22440', # Panther
+        '44996'  # Marshal
+
+    ]
+
+BLOCKADE_RUNNER = [
+        '12733', # Prorator
+        '12729', # Crane
+        '12743', # Viator
+        '12735'  # Prowler
+    ]
+
+CARRIER = [
+        '23757', # Archon
+        '23915', # Chimera
+        '23911', # Thanatos
+        '24483'  # Nidhoggur
+    ]
+
+COMBAT_BATTLECRUISER = [
+        '24696', # Harbinger
+        '16233', # Prophecy
+        '33875', # Prophecy Blood Raiders Edition
+        '24698', # Drake
+        '16227', # Ferox
+        '33873', # Ferox Guristas Edition
+        '16229', # Brutix
+        '33869', # Brutix Serpentis Edition
+        '24700', # Myrmidon
+        '16231', # Cyclone
+        '33871', # Cyclone Thukker Tribe Edition
+        '24702', # Hurricane
+        '33155', # Harbinger Navy Issue
+        '33153', # Drake Navy Issue
+        '33151', # Brutix Navy Issue
+        '33157'  # Hurricane Fleet Issue
+
+    ]
+
+COMBAT_RECON_SHIP = [
+        '20125', # Curse
+        '11959', # Rook
+        '11971', # Lachesis
+        '11961'  # Huginn
+    ]
+
+COMMAND_DESTROYER = [
+        # Pontifex
+        # Stork
+        # Magus
+        # Bifrost
+    ]
+
+COMMAND_SHIP = [
+        '22448', # Absolution
+        '22474', # Damnation
+        '22470', # Nighthawk
+        '22446', # Vulture
+        '22466', # Astarte
+        '22442', # Eos
+        '22468', # Claymore
+        '22444'  # Sleipnir
+    ]
+
+CORVETTE = [
+        '596', # Impairor
+        '601', # Ibis
+        '606', # Velator
+        '588',  # Reaper
+        '617', # Echo
+        '33079', # Hematos
+        '615', # Immolator
+        '33081', # Taipan
+        '33083',  # Violator
+        '9862', # Polaris Centurion Frigate
+        '9858', # Polaris Centurion TEST
+        '1233', # Polaris Enigma Frigate
+        '9854', # Polaris Inspector Frigate
+        '9860'  # Polaris Legatus Frigate
+
+    ]
+
+COVERT_OPS = [
+        '11188', # Anathema
+        '11192', # Buzzard
+        '11172', # Helios
+        '11182', # Cheetah
+        '33397', # Chremoas
+        '44993'  # Pacifier
+    ]
+
+CRUSIER = [
+        '628', # Arbitrator
+        '625', # Augoror
+        '624', # Maller
+        '2006', # Omen
+        '33639', # Omen Kador Edition
+        '33641', # Omen Tash-Murkon Edition
+        '632', # Blackbird
+        '621', # Caracal
+        '33643', # Caracal Nugoeihuvi Edition
+        '33645', # Caracal Wiyrkomi Edition
+        '623', # Moa
+        '620', # Osprey
+        '633', # Celestis
+        '634', # Exequror
+        '25560', # Opux Dragoon Yacht
+        '627', # Thorax
+        '33651', # Thorax Aliastra Edition
+        '33653', # Thorax Inner Zone Shipping Edition
+        '626', # Vexor
+        '630', # Bellicose
+        '629', # Rupture
+        '631', # Scythe
+        '622', # Stabber
+        '33649', # Stabber Krusual Edition
+        '33647', # Stabber Nefantar Edition
+        '1904', # Concord Police Cruiser
+        '11011', # Guardian-Vexor
+        '635', # Opux Luxury Yacht
+        '29337', # Augoror Navy Issue
+        '17709', # Omen Navy Issue
+        '17634', # Caracal Navy Issue
+        '29340', # Osprey Navy Issue
+        '29344', # Exequror Navy Issue
+        '17843', # Vexor Navy Issue
+        '29336', # Scythe Fleet Issue
+        '17713',  # Stabber Fleet Issue
+        '17720', # Cynabal
+        '17922', # Ashimmu
+        '17715', # Gila
+        '33818', # Orthrus
+        '17718', # Phantasm
+        '17722', # Vigilant
+        '33470',  # Stratios
+        '33553'  # Stratios Emergency Responder
+    ]
+
+DEEP_SPACE_TRANSPORT = [
+        '12753', # Impel
+        '12731', # Bustard
+        '12745', # Occator
+        '12747'  # Mastodon
+    ]
+
+DESTROYER = [
+        '16236', # Coercer
+        '33879', # Coercer Blood Raiders Edition
+        '32874', # Dragoon
+        '32876', # Corax
+        '16238', # Cormorant
+        '33881', # Cormorant Guristas Edition
+        '32872', # Algos
+        '16240', # Catalyst
+        '33877', # Catalyst Serpentis Edition
+        '32878', # Talwar
+        '16242', # Thrasher
+        '33883', # Thrasher Thukker Tribe Edition
+        '32848', # Aliastra Catalyst
+        '32844', # Inner Zone Shipping Catalyst
+        '32842', # Intaki Syndicate Catalyst
+        '32840', # InterBus Catalyst
+        '33099', # Nefantar Thrasher
+        '32846', # Quafe Catalyst
+        '42685'  # Sunesis
+    ]
+
+DREADNOUGHT = [
+        '19720', # Revelation
+        '34345', # Revelation Sarum Edition
+        '19726', # Phoenix
+        '34343', # Phoenix Wiyrkomi Edition
+        '19724', # Moros
+        '34339', # Moros Interbus Edition
+        '19722', # Naglfar
+        '34341', # Naglfar Justice Edition
+        '42243', # Chemosh
+        '45647'  # Caiman
+
+    ]
+
+ELECTRONIC_ATTACK_SHIP = [
+        '11190', # Sentinel
+        '11194', # Kitsune
+        '11174', # Keres
+        '11387' # Hyena
+    ]
+
+FORCE_AUXILIARY = [
+        '42242', # Dagon
+        '45645', # Loggerhead
+        '37604', # Apostle
+        '37605', # Minokawa
+        '37607', # Ninazu
+        '37606'  # Lif
+    ]
+
+FORCE_RECON_SHIP = [
+        '11965', # Pilgrim
+        '11957', # Falcon
+        '11969', # Arazu
+        '11963', # Rapier
+        '33675', # Chameleon
+        '363310', # Enforcer
+        '33395' # Moracha
+    ]
+
+FREIGHTER = [
+        '20183', # Providence
+        '20185', # Charon
+        '20187', # Obelisk
+        '20189', # Fenrir
+        '34328'  # Bowhead
+    ]
+
+FRIGATE = [
+        '2161', # Crucifier
+        '37453', # Crucifier Navy Issue
+        '589', # Executioner
+        '590', # Inquisitor
+        '29248', # Magnate
+        '597', # Punisher
+        '33655', # Punisher Kador Edition
+        '33657', # Punisher Tash-Murkon Edition
+        '591', # Tormentor
+        '582', # Bantam
+        '583', # Condor
+        '584', # Griffin
+        '37455', # Griffin Navy Issue
+        '605', # Heron
+        '602', # Kestrel
+        '603', # Merlin
+        '33659', # Merlin Nugoeihuvi Edition
+        '33661', # Merlin Wiyrkomi Edition
+        '608', # Atron
+        '607', # Imicus
+        '594', # Incursus
+        '33667', # Incursus Aliastra Edition
+        '33669', # Incursus Inner Zone Shipping Edition
+        '609', # Maulus
+        '37456', # Maulus Navy Issue
+        '592', # Navitas
+        '593', # Tristan
+        '598', # Breacher
+        '599', # Burst
+        '586', # Probe
+        '587', # Rifter
+        '33665', # Rifter Krusual Edition
+        '33663', # Rifter Nefantar Edition
+        '585', # Slasher
+        '3766', # Vigil
+        '37454', # Vigil Fleet Issue
+        '32880', # Venture
+        '3768', # Amarr Police Frigate
+        '1900', # Concord Army Frigate
+        '1896', # Concord Police Frigate
+        '1902', # Concord Special Ops Frigate
+        '1898', # Concord SWAT Frigate
+        '613', # Devourer
+        '3532', # Echelon
+        '614', # Fury
+        '595', # Gallente Police Ship
+        '11940', # Gold Magnate
+        '32985', # Inner Zone Shipping Imicus
+        '17705', # Khanid Navy Frigate
+        '618', # Lynx
+        '616', # Medusa
+        '600', # Minmatar Peacekeeper Ship
+        '17707', # Mordus Frigate
+        '32987', # Sarum Magnate
+        '11942', # Silver Magnate
+        '32983', # Sukuuvestaa Heron
+        '619', # Swordspine
+        '33190', # Tash-Murkon Magnate
+        '32989', # Vherokior Probe
+        '11019', # Cockroach
+        '17360', # Immovable Enigma
+        '17703', # Imperial Navy Slicer
+        '17619', # Caldari Navy Hookbill
+        '17841', # Federation Navy Comet
+        '33677', # Police Pursuit Comet
+        '17812', # Republic Fleet Firetail
+        '17932', # Dramiel
+        '17926', # Cruor
+        '33816', # Garmur
+        '17924', # Succubus
+        '17928', # Daredevil
+        '33468'  # Astero
+
+    ]
+
+HEAVY_ASSAULT_CRUISER = [
+        '12019', # Sacrilege
+        '12003', # Zealot
+        '11993', # Cerberus
+        '12011', # Eagle
+        '12023', # Deimos
+        '12005', # Ishtar
+        '12015', # Muninn
+        '11999', # Vagabond
+        '2836', # Adrestia
+        '32209', # Mimir
+        '3518' # Vangel
+    ]
+
+HEAVY_INTERDICTION_CRUISER = [
+        '12017', # Devoter
+        '11995', # Onyx
+        '12021', # Phobos
+        '12013'  # Broadsword
+    ]
+
+INDUSTRIAL = [
+        '1944', # Bestower
+        '33695', # Bestower Tash-Murkon Edition
+        '19744', # Sigil
+        '648', # Badger
+        '649', # Tayra
+        '33691', # Tayra Wiyrkomi Edition
+        '655', # Epithal
+        '33689', # Iteron Inner Zone Shipping Edition
+        '657', # Iteron Mark V
+        '654', # Kryos
+        '656', # Miasmos
+        '650', # Nereus
+        '651', # Hoarder
+        '652', # Mammoth
+        '33693', # Mammoth Nefantar Edition
+        '653',  # Wreathe
+        '2998', # Noctis
+        '32811', # Miasmos Amastris Edition
+        '4363', # Miasmos Quafe Ultra Edition
+        '4388', # Miasmos Quafe Ultramarine Edition
+        '2863' # Primae
+    ]
+
+INTERCEPTOR = [
+        '11184', # Crusader
+        '11186', # Malediction
+        '11176', # Crow
+        '11178', # Raptor
+        '11202', # Ares
+        '11200', # Taranis
+        '11196', # Claw
+        '11198', # Stiletto
+        '33673'  # Whiptail
+
+    ]
+
+INTERDICTOR = [
+        '22452', # Heretic
+        '22464', # Flycatcher
+        '22460', # Eris
+        '22456'  # Sabre
+    ]
+
+JUMP_FREIGHTER = [
+        '28850', # Ark
+        '28844', # Rhea
+        '28848', # Anshar
+        '28846'  # Nomad
+    ]
+
+LOGISTICS = [
+        '11987', # Guardian
+        '11985', # Basilisk
+        '11989', # Oneiros
+        '11978', # Scimitar
+        '32790'  # Etana
+
+    ]
+
+MARAUDER = [
+        '28659', # Paladin
+        '34219', # Paladin Blood Raider Edition
+        '34221', # Paladin Kador Edition
+        '34223', # Paladin Tash-Murkon Edition
+        '28710', # Golem
+        '34231', # Golem Guristas Edition
+        '34233', # Golem Kaalakiota Edition
+        '34235', # Golem Nugoeihuvi Edition
+        '28661', # Kronos
+        '34245', # Kronos Inner Zone Shipping Edition
+        '34241', # Kronos Police Edition
+        '34243', # Kronos Quafe Edition
+        '28665', # Vargur
+        '34253', # Vargur Justice Edition
+        '34255', # Vargur Krusual Edition
+        '34257'  # Vargur Nefantar Edition
+    ]
+
+SHUTTLE = [
+        '11134', # Amarr Shuttle
+        '672',   # Caldari Shuttle
+        '11129', # Gallente Shuttle
+        '11132', # Minmatar Shuttle
+        '21097', # Goru's Shuttle
+        '30842', # InterBus Shuttle
+        '33513', # Leopard
+        '29328', # Amarr Media Shuttle
+        '29330', # Caldari Media Shuttle
+        '27299', # Civilian Amarr Shuttle
+        '27301', # Civilian Caldari Shuttle
+        '27303', # Civilian Gallente Shuttle
+        '27305', # Civilian Minmatar Shuttle
+        '29332', # Gallente Media Shuttle
+        '29334', # Minmatar Media Shuttle
+        '21628'  # Guristas Shuttle
+    ]
+
+STEALTH_BOMBER = [
+        '12038', # Purifier
+        '12032', # Manticore
+        '11377', # Nemesis
+        '12034'  # Hound
+    ]
+
+STRATEGIC_CRUISER = [
+        '29986', # Legion
+        '29984', # Tengu
+        '29988', # Proteus
+        '29990'  # Loki
+    ]
+
+SUPERCARRIER = [
+        '23919', # Aeon
+        '23917', # Wyvern
+        '23913', # Nyx
+        '22852', # Hel
+        '3628',  # Nation
+        '3514'   # Revenant
+    ]
+
+TACTICAL_DESTROYER = [
+        '34317', # Confessor
+        '34828', # Jackdaw
+        '35683', # Hecate
+        '34562'  # Svipul
+    ]
+
+TITAN = [
+        '11567', # Avatar
+        '3764', # Leviathan
+        '671', # Erebus
+        '23773', # Ragnarok
+        '45649' # Komodo
+
+    ]
+
+# ORE SHIPS
+CAPITAL_INDUSTRIAL_SHIP = [
+        '28352', # Rorqual
+        '33687'  # Rorqual ORE Development Edition
+    ]
+
+EXHUMER = [
+        '22544', # Hulk
+        '22548', # Mackinaw
+        '33683', # Mackinaw ORE Development Edition
+        '22546'  # Skiff
+    ]
+
+EXPEDITION_FRIGATE = [
+        '353428', # Endurance
+        '33697'  # Prospect
+    ]
+
+INDUSTRIAL_COMMAND_SHIP = [
+        '28606', # Orca
+        '33685', # Orca ORE Development Edition
+        '42244'  # Porpoise
+    ]
+
+MINING_BARGE = [
+        '17476', # Covetor
+        '17480', # Procurer
+        '17478'  # Retriever
+    ]
+
+PROTOTYPE_EXPLORATION_SHIP = [
+        '2078' # Zephyr
+    ]
+
+CAPSULE = [
+        '670', # Capsule
+        '33328' # Capsule - Genolution 'Auroral' 197-variant
+    ]
+
+FLAG_CRUISER = [
+        '45534' # Monitor
+    ]
+
+
+if __name__ == '__main__':
+    from typeid import TypeID
+    typeid = TypeID(from_cache=True).get()
+    typeid_inv = {v: k for k, v in typeid.items()}
+
+    while(True):
+        a = input('')
+        try:
+            print('\'%s\', # %s' % (typeid_inv[a], a))
+        except KeyError:
+            print('%s not found' % a)
--- src/typeid.py
+++ src/typeid.py
@@ -2,6 +2,8 @@
 import urllib
 import requests
 import util
+import bz2
+import csv
 from util import DIR_PATH
 
 class TypeID:
@@ -9,7 +11,7 @@ class TypeID:
         Class that loads the typeid list from online source.
         Provides options to cache local copy for fast debugging.
     '''
-    def __init__(self, url='http://eve-files.com/chribba/typeid.txt', file=DIR_PATH + 'lib/pickled/typeid.p', from_cache=False, cache=True):
+    def __init__(self, url='https://www.fuzzwork.co.uk/dump/latest/invTypes.csv.bz2', file=DIR_PATH + 'lib/pickled/typeid.p', from_cache=False, cache=True):
         self.url = url
         self.file = file
 
@@ -22,23 +24,17 @@ class TypeID:
             self.__to_file()
 
     def __get(self):
-        response = urllib.request.urlopen(self.url)
-        if response.getcode() != 200:
+        response = requests.get(self.url)
+        if response.status_code != 200:
             raise Exception("Recieved status code %d from %s" % (response.getcode(), self.url))
 
+        # decompress and convert to csv iterable
+        bz2_obj = bz2.decompress(response.content)
+        reader = csv.DictReader(bz2_obj.decode('utf-8').split('\n'), delimiter=',')
+
         data = {} # eve entity lookup table
-        for i, line in enumerate(response):
-            # skip lines 0 and 1
-            if i in [0, 1]:
-                continue
-            # process into a dictionary
-            else:
-                line = line.decode("utf-8").split()
-                # check valid line
-                if len(line) < 2:
-                    continue
-                else:
-                    data.update({line[0]: " ".join(line[1:])})
+        for line in reader:
+            data.update({line['typeID'] : line['typeName']})
 
         return data
 
@@ -50,3 +46,6 @@ class TypeID:
 
     def get(self):
         return self.data
+
+if __name__ == '__main__':
+    a = TypeID(from_cache=False)
